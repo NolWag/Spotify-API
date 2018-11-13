ec5e4a167d40a851e22871c1ee05ea1f381b7cc3
NolWag <NolanDubyu@gmail.com>
2018-11-12 18:38:26 -0600

    initial

---
+++ routes.js
@@ -0,0 +1,120 @@
+var express = require("express");
+var Spotify = require('node-spotify-api');
+var User = require("./models/user");
+var passport = require("passport");
+
+var router = express.Router();
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.flash("info", "You must be logged in to see this page.");
+    res.redirect("/login");
+  }
+}
+
+router.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.errors = req.flash("error");
+  res.locals.infos = req.flash("info");
+  next();
+});
+
+router.get("/", function(req, res, next) {
+  User.find()
+  .sort({ createdAt: "descending" })
+  .exec(function(err, users) {
+    if (err) { return next(err); }
+    res.render("index", { users: users });
+  });
+});
+
+router.get("/login", function(req, res) {
+  res.render("login");
+});
+
+router.post("/login", passport.authenticate("login", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true
+}));
+
+router.get('/music', function(req, res) {
+
+  var spotify = new Spotify({
+    id: '4ef8dde5a73e4b1783a63138c6ea808f',
+    secret: '5fe6831b10d74122b87afc415d2cf10e'
+  });
+
+  spotify.request('https://api.spotify.com/v1/browse/categories')
+  .then(function(response) {
+    console.log(response);
+    res.render('music', {data: response});
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
+});
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+router.get("/signup", function(req, res) {
+  res.render("signup");
+});
+
+router.post("/signup", function(req, res, next) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({ username: username }, function(err, user) {
+
+    if (err) { return next(err); }
+    if (user) {
+      req.flash("error", "User already exists");
+      return res.redirect("/signup");
+    }
+
+    var newUser = new User({
+      username: username,
+      password: password
+    });
+    newUser.save(next);
+
+  });
+}, passport.authenticate("login", {
+  successRedirect: "/",
+  failureRedirect: "/signup",
+  failureFlash: true
+}));
+
+router.get("/users/:username", function(req, res, next) {
+  User.findOne({ username: req.params.username }, function(err, user) {
+    if (err) { return next(err); }
+    if (!user) { return next(404); }
+    res.render("profile", { user: user });
+  });
+});
+
+router.get("/edit", ensureAuthenticated, function(req, res) {
+  res.render("edit");
+});
+
+router.post("/edit", ensureAuthenticated, function(req, res, next) {
+  req.user.displayName = req.body.displayname;
+  req.user.bio = req.body.bio;
+  req.user.save(function(err) {
+    if (err) {
+      next(err);
+      return;
+    }
+    req.flash("info", "Profile updated!");
+    res.redirect("/edit");
+  });
+});
+
+module.exports = router;
